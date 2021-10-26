444013fcdc3051e4c81524c474e34dc7e19b3dd2
pearl-kjj <pearls0803@naver.com>
2021-10-26 16:58:28 +0900

    for 2

---
+++ JavaProject/days07/ControllOpFor10.java
@@ -0,0 +1,22 @@
+package days07;
+
+import java.util.Scanner;
+
+public class ControllOpFor10 {
+
+	public static void main(String[] args) {
+		int sum=0, i, num;
+		Scanner sc = new Scanner(System.in);
+		// 10개의 정수를 입력받아 그들의 합계를 출력하세요
+		// 10개를 입력하는 중간에 종료를 원하면 0입력하여 종료, 최종합계 출력
+		//num = sc.nextInt();
+		for(i=0; i<10; i++) {
+			System.out.printf("입력한 정수%d : ", i+1);
+			num = sc.nextInt();
+			sum += num;
+			if(num ==0) break;		
+		}
+		System.out.printf("\n입력한 정수들의 합계: %d", sum);
+	}  // 순서를 잘 못 생각함  num과 System~ 문장 순서가 바뀌어있어서 원하는대로 출력안됐음
+
+}
---
+++ JavaProject/days07/ControllOpFor14.java
@@ -0,0 +1,24 @@
+package days07;
+
+public class ControllOpFor14 {
+
+	public static void main(String[] args) {
+		// 이중 반복문을 이용, 구구단 1단부터 9단까지 출력
+		// 한개의 단이 출력되는 방향은 가로 세로 상관없음
+		int a, b;
+	for(a=2; a<=9; a++) {
+		System.out.printf("=" + a + "단 구구단=");
+		for(b=1; b<=9; b++) {
+		System.out.println(a + " x " + b + " = " + a*b);
+	}
+	}  // 내가 한 방법
+	int i, j;
+	for(i=1; i<=9; i++) {
+		for(j=2; j<=9; j++) {
+			System.out.printf("%dx%d=%2d ", j, i, i*j);
+		}
+		System.out.println();
+	} // 강사님 방법  원하는 출력형태에 따라 다를 듯
+	
+	} 
+}
