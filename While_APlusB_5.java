b9c46627b6e7498242033930c7b4a23ab99729e5
jinjoo <93130410+kxmjxnjoo@users.noreply.github.com>
2021-10-30 20:55:48 +0900

    Add files via upload

---
+++ While_APlusB_5.java
@@ -0,0 +1,21 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class While_APlusB_5 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int i;
+		while(true) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			if(A+B==0) break;
+			System.out.println(A+B);
+		}
+	
+	 
+		
+	}
+}
