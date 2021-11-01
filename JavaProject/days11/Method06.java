1c97880f9000ab0e92628b0280f13ed8ddc1cfb9
kxmjxnjoo <pearls0803@naver.com>
2021-11-01 22:32:05 +0900

    Method

--- JavaProject/days11/Method06.java
+++ JavaProject/days11/Method06.java
@@ -6,7 +6,6 @@ public class Method06 {
 		int[] a = {56,54,87,89,25,36,57,98,59,87};
 		// 아래명령 sum(a)이 정상실행되어 배열내 값의 합계를 출력하도록 sum 함수를 제작
 		sum(a);
-
 		} 
 	public static void sum(int[] a) {
 		int total = 0;
