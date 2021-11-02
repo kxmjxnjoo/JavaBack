2958556a7412ccc6647ff7c28cf83ead696b0c07
kxmjxnjoo <pearls0803@naver.com>
2021-11-02 21:36:00 +0900

    Example_gugudan

--- JavaProject/days12/Class12.java
+++ JavaProject/days12/Class12.java
@@ -1,4 +1,6 @@
 package days12;
+
+
 // int형 데이터 하나를 매개변수로 하는 생성자와 디폴트 생성자가 오버로딩 된 생성자를 갖는 클래스 제작
 // 멤버 필드로는 int dan; 하나 생성
 // 멤버 메소드 : dan 값이 0이면 1~9단 모두 출력, 0 이 아니면 해당 구구단만 출력 ptint()
@@ -15,14 +17,14 @@ class Gugudan {
 		public void print() {
 			if(dan==0) {
 			for(int i=2; i<=9; i++) {
-				for(int j=2; j<=9; j++) {
+				for(int j=1; j<=9; j++) {
 					System.out.printf("%dx%d=%d\n" , i, j, i*j);
 				}
 			    System.out.println();
 				}
 			}else {
 			for(int j=1; j<=9; j++) {
-				System.out.printf("%dx%d=%d ",dan, j, dan*j);
+				System.out.printf("%dx%d=%d\n",dan, j, dan*j);
 			}
 		}
     }
@@ -33,6 +35,7 @@ public class Class12 {
 		Gugudan g1 = new Gugudan(8);
 		Gugudan g2 = new Gugudan();
 		g1.print();
+		System.out.println();
 		g2.print();
 
 	}
