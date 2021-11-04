0cf1d8652b9fefcd4ea589e5cdd1f5915e60b991
kxmjxnjoo <pearls0803@naver.com>
2021-11-04 13:45:51 +0900

    Singleton

---
+++ JavaProject/days14/Class26.java
@@ -0,0 +1,13 @@
+package days14;
+class SingletonEx {
+	
+}
+public class Class26 {
+
+	public static void main(String[] args) {
+		// SingletonEx s1 = new SingletonEx();
+		// SingletonEx s2 = new SingletonEx();
+		// 싱클톤 방식이란 위의 s1, s2의 레퍼런스 변수가 같은 주소를 같게하는 방식을 말함
+		// 현재는 new 연산자 사용했기 때문에 다른 주소를 가지고 있음 
+	}
+}
