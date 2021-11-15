f62519de6c310737984f73b6fab2264ce68319b1
kxmjxnjoo <pearls0803@naver.com>
2021-11-15 11:32:11 +0900

    Window

--- JavaProject/days19/SwingEx.java
+++ JavaProject/days19/SwingEx.java
@@ -10,10 +10,10 @@ class WindowTest extends JFrame{
 		setVisible(true);  // 생성된 윈도우의 화면 표시
 	}
 }
-
 public class SwingEx {
 
 	public static void main(String[] args) {
-		WindowTest win = new WindowTest();
+		//WindowTest win = new WindowTest();
+		new WindowTest();  //위와같이 작성하지 않고 생성자 호출로 사용가능
 	}
 }
