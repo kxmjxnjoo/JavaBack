b8ee40585ff8664b71e716827a6bc47ce61b1626
kxmjxnjoo <pearls0803@naver.com>
2021-12-20 17:25:57 +0900

    Booklist_Delete

---
+++ Oracle_DataBase/src/jdbc02/Booklist_Delete.java
@@ -0,0 +1,41 @@
+package jdbc02;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Booklist_Delete {
+
+	public static void main(String[] args) {
+		String driver ="oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		Connection con = null; 
+		PreparedStatement pstmt = null; 
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, "scott", "tiger");
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("삭제할 도서의 도서번호를 입력하세요: ");
+			String num = sc.nextLine();
+			
+			String sql = "delete from booklist where num = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, Integer.parseInt(num));
+			
+			int result = pstmt.executeUpdate();
+			if(result ==1) System.out.println("삭제 성공~");
+			else System.out.println("삭제 실패b");
+		
+		} catch (ClassNotFoundException e) {e.printStackTrace();
+		} catch (SQLException e) { e.printStackTrace();
+		}
+		try {
+			if(con !=null ) con.close();
+			if(pstmt != null) pstmt.close();
+		} catch (SQLException e) {e.printStackTrace();
+		}
+	}
+}
