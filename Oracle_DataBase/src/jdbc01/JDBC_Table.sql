95f8be32ece66a83ecf5d0343bc74def66eff4e6
kxmjxnjoo <pearls0803@naver.com>
2021-12-20 17:21:52 +0900

    JDBC_Table

--- Oracle_DataBase/src/jdbc01/JDBC_Table.sql
+++ Oracle_DataBase/src/jdbc01/JDBC_Table.sql
@@ -1,9 +1,10 @@
 create table customer(
 	num number(3),
 	name varchar2(10),
-	email varchar2(20),
-	tel varchar2(20)
+	email varchar2(15),
+	tel varchar2(15)
 );
+alter table customer modify email varchar2(20); 
 
 insert into customer values(1, '홍길동', 'abc1@abc.com','010-1234-5234');
 insert into customer values(2, '홍길서', 'abc2@abc.com','010-2234-4234');
@@ -12,3 +13,5 @@ insert into customer values(4, '홍길북', 'abc4@abc.com','010-4234-2234');
 insert into customer values(5, '아무개', 'abc5@abc.com','010-5234-1234');
 
 select * from customer
+
+create sequence num_seq start with 7 increment by 1
