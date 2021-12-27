04fffda3a8303ba9879cf4eecb67c4b8658db7f7
kxmjxnjoo <pearls0803@naver.com>
2021-12-27 17:54:04 +0900

    Dbman

---
+++ MySQL_DataBase/src/jdbc01/Dbman.java
@@ -0,0 +1,36 @@
+package jdbc01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Dbman {
+
+	private static String driver = "com.mysql.cj.jdbc.Driver";
+	private static String url = "jdbc:mysql://localhost:3306/scott";
+	private static String id="root";
+	private static String pw = "adminuser";
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, id, pw);
+		} catch (ClassNotFoundException e) { 	e.printStackTrace();
+		} catch (SQLException e) { 	e.printStackTrace();		}
+		return conn;
+	}
+	
+
+public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+		
+		try {
+			if(con != null) con.close();
+			if(pstmt != null) pstmt.close();
+			if(rs != null) rs.close();
+		} catch(SQLException e) {e.printStackTrace();
+		}
+	}
+}
